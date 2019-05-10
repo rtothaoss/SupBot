83798106de5281fb072f8124eafc5570cbd274e2
Ross carmack <rosscarmack@Rosss-MBP.fios-router.home>
2019-05-10 18:53:39 -0500

    added working modal for upcoming drop page. next up will be bot

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -10,7 +10,7 @@ const Card = (props) => {
   <div className="card-body">
     <h5 className="card-title">{props.title}</h5>
     <p className="card-text">{props.summary}</p>
-    {/* <a href={props.linkURL} className="btn btn-primary">{props.linkName}</a> */}
+    <button onClick={props.onClick} className="btn btn-primary">More Info</button>
   </div>
 </div>
     )
--- client/src/pages/UpcomingDrop.js
+++ client/src/pages/UpcomingDrop.js
@@ -3,7 +3,7 @@ import Jumbotron from "../components/Jumbotron";
 import Card from "../components/Card";
 import { Col, Row, Container } from "../components/Grid";
 import API from "../utils/API";
-import Modal from "../components/Modal";
+import Modal from 'react-bootstrap4-modal'
 
 
 const styles = {
@@ -27,14 +27,19 @@ class UpcomingDrop extends Component {
 
     state = {
         items: [],
-        modal: ''
+        modal: '',
+        visible: false,
+        summary: '',
+        id: '',
+        title: '',
+        img: ''
     }
 
     componentDidMount() {
         this.deleteDropList()
         this.scrapeSup();
-        setTimeout(() => {this.droplistLoaded()}, 2500)
-  
+        setTimeout(() => { this.droplistLoaded() }, 2500)
+
     }
 
     scrapeSup = () => {
@@ -61,34 +66,63 @@ class UpcomingDrop extends Component {
                 this.setState({
                     items: res.data
                 }))
-            .catch(() => {console.log('error')
+            .catch(() => {
+                console.log('error')
             }, () => {
                 console.log(this.state.items)
             })
-            
+
     }
 
+    viewModal = (item) => {
+        if(!this.state.visible) {
+            console.log(item)
+            this.setState({
+                visible: true,
+                id: item.id,
+                summary: item.summary,
+                title: item.title,
+                img: 'https://www.supremecommunity.com' + item.img
+            })
+        } else {
+            this.setState({
+                visible: false
+            })
+        }
+    }
 
     render() {
         return (
             <Container>
-            <Jumbotron style={styles.jumbotron}>
-                <h3>Next drop in: ...</h3>
-            </Jumbotron>
-            <Jumbotron>
-            </Jumbotron>
                 <Jumbotron style={styles.jumbotron}>
+                <h3>Next drop in: ...</h3>
+                <Modal visible={this.state.visible} onClickBackdrop={this.modalBackdropClicked}>
+                        <div className="modal-header">
+                            <h5 className="modal-title">{this.state.title}</h5>
+                        </div>
+                        <div className="modal-body">
+                        <img src={this.state.img} alt={this.state.title}/>
+                            <p>{this.state.summary}</p>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-primary" onClick={this.viewModal}>
+                                Close
+                        </button>
+                        </div>
+                    </Modal>
                     <Row>
-                    {this.state.items.map(item => (
-                        <Col size="md-4">
-                            <Card style={styles.card}
-                                key={item._id}
-                                title={item.title}
-                                img={'https://www.supremecommunity.com' + item.img}
-                                summary={item.summary}
-                            />
-                        </Col>
-                    ))}
+                        {this.state.items.map(item => (
+                            <Col size="md-4">
+                                <Card style={styles.card}
+                                    key={item._id}
+                                    id={item._id}
+                                    title={item.title}
+                                    img={'https://www.supremecommunity.com' + item.img}
+                                    summary={item.summary}
+                                    onClick={()=>{this.viewModal(item)}}
+                                />
+                            </Col>
+                        ))}
                     </Row>
                 </Jumbotron>
             </Container>
